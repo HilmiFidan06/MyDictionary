6623d317fa3ce4bbf2b8ca2e6295365f88fe60bd
HilmiFidan06 <hlm14530671@gmail.com>
2021-03-30 20:23:56 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDictionary<string> Product = new ProductDictionary<string>();
+            Product.Add("Fındık");
+
+            Console.WriteLine("Liste Eleman Sayısı : " + Product.Length);
+
+            Product.Add("Antep Fıstığı");
+            Product.Add("Kaju");
+
+            Console.WriteLine("Liste Eleman Sayısı : " + Product.Length);
+
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("Ürün Listesi İçeriği");
+            foreach (var product in Product.Items)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
